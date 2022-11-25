5d3591412aab13ff9eed3a4ca5f54dd375ef9268
wuchengxing <1449697757@qq.com>
2022-11-25 17:10:54 +0800

    feat：初始化算法仓库

---
+++ src/main/java/com/simple/algorithm/AlgorithmMainApp.java
@@ -0,0 +1,15 @@
+package com.simple.algorithm;
+
+/**
+ * 项目: algorithm
+ * <p>
+ * 功能描述:
+ *
+ * @author: WuChengXing
+ * @create: 2022-11-25 17:09
+ **/
+public class AlgorithmMainApp {
+    public static void main(String[] args) {
+
+    }
+}
