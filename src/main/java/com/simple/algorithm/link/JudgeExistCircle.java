e18df07aad8e76705184023911c5b712b0dc0392
“simple” <1449697757@qq.com>
2022-12-11 22:29:43 +0800

    feat：判断链表是否成环

--- src/main/java/com/simple/algorithm/link/JudgeExistCircle.java
+++ src/main/java/com/simple/algorithm/link/JudgeExistCircle.java
@@ -7,26 +7,28 @@ import org.junit.Test;
 /**
  * 项目: algorithm
  * <p>
- * 功能描述: 找到中间节点
+ * 功能描述: 判断链表是否成环
  *
  * @author: WuChengXing
- * @create: 2022-12-11 21:49
+ * @create: 2022-12-11 22:04
  **/
-public class FindMidNode {
+public class JudgeExistCircle {
 
     @Test
     public void testResult() {
-        LinkNode<Integer> linkNode = NodeInit.initNode();
-        System.out.println(findMidNode(linkNode));
+        LinkNode<Integer> linkNode = NodeInit.initNodeCycle();
+        System.out.println(existCircle(linkNode));
     }
 
-    public Integer findMidNode(LinkNode<Integer> sourceNode) {
+    public boolean existCircle(LinkNode<Integer> sourceNode) {
         LinkNode<Integer> fast = sourceNode, slow = sourceNode;
         while (fast != null && fast.next != null) {
-            // 快指针走两步
             fast = fast.next.next;
             slow = slow.next;
+            if (fast.val.equals(slow.val)) {
+                return true;
+            }
         }
-        return slow.val;
+        return false;
     }
 }
