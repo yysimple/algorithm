e978e38e223f4e89289201ce6bddf52d6952c889
wuchengxing <1449697757@qq.com>
2022-12-12 19:28:48 +0800

    feat：找到环节点

--- src/main/java/com/simple/algorithm/link/FindCircleNode.java
+++ src/main/java/com/simple/algorithm/link/FindCircleNode.java
@@ -1,5 +1,7 @@
 package com.simple.algorithm.link;
 
+import com.simple.algorithm.common.init.NodeInit;
+import com.simple.algorithm.common.model.LinkNode;
 import org.junit.Test;
 
 /**
@@ -14,7 +16,30 @@ public class FindCircleNode {
 
     @Test
     public void testResult() {
+        LinkNode<Integer> x = NodeInit.initNodeCycle();
+        System.out.println(findCircleNode(x).val);
+        System.out.println(LinkNode.printNode(findCircleNode(x)));
+    }
 
+    public LinkNode<Integer> findCircleNode(LinkNode<Integer> node) {
+        LinkNode<Integer> fast = node, slow = node;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                break;
+            }
+        }
+        if (fast == null || fast.next == null) {
+            return null;
+        }
+        // 这里将slow置为node节点即可，此时快节点到相交节点的距离和头到节点的距离是一样的
+        slow = node;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
     }
 
 }
